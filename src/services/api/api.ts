ccf4fb56b894d031e73209b858034ddc6224c949
Matthias Schott <matthias.schott@gmx.net>
2025-01-27 14:21:51 +0100

    feat: improve api typings

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -1,4 +1,11 @@
-import { type Film } from "@/types";
+import {
+  type Film,
+  type People,
+  type Planet,
+  type Species,
+  type Starship,
+  type Vehicle,
+} from '@/types';
 
 export type PaginatedResponse<T = unknown> = {
   count: number;
@@ -12,9 +19,9 @@ export type PaginatedResponse<T = unknown> = {
  */
 const apiFetch = async <ResponseType>(
   input: string | URL,
-  init?: RequestInit
+  init?: RequestInit,
 ): Promise<ResponseType> => {
-  const baseUrl = "https://swapi.dev/api";
+  const baseUrl = 'https://swapi.dev/api';
   const path = input;
   const response = await fetch(baseUrl + path, init);
   const data = await response.json();
@@ -22,9 +29,21 @@ const apiFetch = async <ResponseType>(
   return data;
 };
 
-export const films = () => apiFetch<PaginatedResponse<Film>>("/films/");
-export const people = () => apiFetch<PaginatedResponse>("/people/");
-export const planets = () => apiFetch<PaginatedResponse>("/planets/");
-export const species = () => apiFetch<PaginatedResponse>("/species/");
-export const starships = () => apiFetch<PaginatedResponse>("/starships/");
-export const vehicles = () => apiFetch<PaginatedResponse>("/vehicles/");
+export const films = () => {
+  return apiFetch<PaginatedResponse<Film>>('/films/');
+};
+export const people = () => {
+  return apiFetch<PaginatedResponse<People>>('/people/');
+};
+export const planets = () => {
+  return apiFetch<PaginatedResponse<Planet>>('/planets/');
+};
+export const species = () => {
+  return apiFetch<PaginatedResponse<Species>>('/species/');
+};
+export const starships = () => {
+  return apiFetch<PaginatedResponse<Starship>>('/starships/');
+};
+export const vehicles = () => {
+  return apiFetch<PaginatedResponse<Vehicle>>('/vehicles/');
+};
--- src/types/resource.ts
+++ src/types/resource.ts
@@ -1,10 +1,10 @@
 export type Resource =
-  | "films"
-  | "people"
-  | "planets"
-  | "species"
-  | "starships"
-  | "vehicles";
+  | 'films'
+  | 'people'
+  | 'planets'
+  | 'species'
+  | 'starships'
+  | 'vehicles';
 
 export type Film = {
   characters: string[];
@@ -21,4 +21,98 @@ export type Film = {
   title: string;
   url: string;
   vehicles: string[];
+};
+
+export type People = {
+  birth_year: string;
+  created: string;
+  edited: string;
+  eye_color: string;
+  films: string[];
+  gender: string;
+  hair_color: string;
+  height: string;
+  homeworld: string;
+  mass: string;
+  name: string;
+  skin_color: string;
+  species: string[];
+  starships: string[];
+  url: string;
+  vehicles: string[];
+};
+
+export type Planet = {
+  climate: string;
+  created: string;
+  diameter: string;
+  edited: string;
+  films: string[];
+  gravity: string;
+  name: string;
+  orbital_period: string;
+  population: string;
+  residents: string[];
+  rotation_period: string;
+  surface_water: string;
+  terrain: string;
+  url: string;
+};
+
+export type Species = {
+  average_height: string;
+  average_lifespan: string;
+  classification: string;
+  created: string;
+  designation: string;
+  edited: string;
+  eye_colors: string;
+  films: string[];
+  hair_colors: string;
+  homeworld: string;
+  language: string;
+  name: string;
+  people: string[];
+  skin_colors: string;
+  url: string;
+};
+
+export type Starship = {
+  MGLT: string;
+  cargo_capacity: string;
+  consumables: string;
+  cost_in_credits: string;
+  created: string;
+  crew: string;
+  edited: string;
+  films: string[];
+  hyperdrive_rating: string;
+  length: string;
+  manufacturer: string;
+  max_atmosphering_speed: string;
+  model: string;
+  name: string;
+  passengers: string;
+  pilots: string[];
+  starship_class: string;
+  url: string;
+};
+
+export type Vehicle = {
+  cargo_capacity: string;
+  consumables: string;
+  cost_in_credits: string;
+  created: string;
+  crew: string;
+  edited: string;
+  films: string[];
+  length: string;
+  manufacturer: string;
+  max_atmosphering_speed: string;
+  model: string;
+  name: string;
+  passengers: string;
+  pilots: string[];
+  url: string;
+  vehicle_class: string;
 };
